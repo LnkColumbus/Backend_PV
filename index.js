49c54435d93d5ed0ae058091ff17df387d3a9d63
LnkColumbus <marlonveliz.nero19@gmail.com>
2020-05-18 22:38:36 -0600

    USER MOVIES, AUTH, SIGN-IN AND SIGN-UP

--- index.js
+++ index.js
@@ -4,21 +4,31 @@ const app = express();
 const bodyParser = require('body-parser');
 
 const { config } = require('./config/index');
+const authApi = require('./routes/auth');
 const moviesApi = require('./routes/movies');
+const userMoviesApi = require('./routes/userMovies');
+const {
+  logErrors,
+  wrapErrors,
+  errorHandler,
+} = require('./utils/middleware/errorHandlers');
 
 const notFoundHandler = require('./utils/middleware/notFoundHandler');
-const { logErrors, wrapErrors, errorHandler } = require('./utils/middleware/errorHandlers');
 
 // body parser
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
 
 // body parser
 app.use(bodyParser.json());
 
 // routes
+authApi(app); // ruta de autenticacion
 moviesApi(app);
+userMoviesApi(app);
 
 // Catch 404
 app.use(notFoundHandler);
@@ -28,7 +38,9 @@ app.use(logErrors);
 app.use(wrapErrors);
 app.use(errorHandler);
 
-
-app.listen(config.dbPort, function() {
-    console.log('Listening http://localhost: \x1b[32m%s\x1b[0m', `${config.dbPort}`);
+app.listen(config.dbPort, function () {
+  console.log(
+    'Listening http://localhost: \x1b[32m%s\x1b[0m',
+    `${config.dbPort}`
+  );
 });
